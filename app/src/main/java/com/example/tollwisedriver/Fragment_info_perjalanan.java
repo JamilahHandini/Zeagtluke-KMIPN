dead6b038ab16a9c522b97d2c0b0dc18aaed8cd8
unknown <jamilahhandini8@gmail.com>
2022-07-20 01:54:18 -0700

    yang ikbal belum aku satuin

--- app/src/main/java/com/example/tollwisedriver/Fragment_info_perjalanan.java
+++ app/src/main/java/com/example/tollwisedriver/Fragment_info_perjalanan.java
@@ -1,19 +1,28 @@
 package com.example.tollwisedriver;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 public class Fragment_info_perjalanan extends Fragment {
 
-    public Fragment_info_perjalanan() {
-        // Required empty public constructor
-    }
+    private LineChart chart;
 
+    View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,141 @@ public class Fragment_info_perjalanan extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_info_perjalanan, container, false);
-        return  v;
+        view = inflater.inflate(R.layout.fragment_info_perjalanan, container, false);
+        chart = view.findViewById(R.id.chart);
+
+
+        chart.getDescription().setEnabled(true);
+
+        chart.setTouchEnabled(true);
+
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawGridBackground(false);
+
+        chart.setPinchZoom(true);
+
+        chart.setBackgroundColor(Color.TRANSPARENT);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        chart.setData(data);
+
+        Legend l = chart.getLegend();
+
+        l.setForm(Legend.LegendForm.LINE);
+        //  l.setTypeface(tfLight);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = chart.getXAxis();
+        // xl.setTypeface(tfLight);
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        //  leftAxis.setTypeface(tfLight);
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        return  view;
     }
+
+    private void addEntry() {
+        LineData data = chart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+
+            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 40) + 30f), 0);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            chart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            chart.setVisibleXRangeMaximum(300);
+            // chart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            chart.moveViewToX(data.getEntryCount());
+
+        }
+    }
+
+    private LineDataSet createSet() {
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(ColorTemplate.getHoloBlue());
+        set.setCircleColor(Color.WHITE);
+        set.setLineWidth(2f);
+        set.setFillAlpha(65);
+        set.setColor(Color.WHITE);
+
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(9f);
+
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+
+        return set;
+    }
+
+    private Thread thread;
+
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i =0 ; i < 100; i++){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+
 }
--- app/src/main/java/com/example/tollwisedriver/MainActivity.java
+++ app/src/main/java/com/example/tollwisedriver/MainActivity.java
@@ -5,15 +5,20 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
     ImageButton back;
     CardView next;
     TextView info;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +27,27 @@ public class MainActivity extends AppCompatActivity {
 
         back = findViewById(R.id.arrow_back);
         next = findViewById(R.id.next);
-        info = findViewById(R.id.real_time_p);
+        info = findViewById(R.id.action_text);
+
+
+
+
     }
 
     public void back(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
+
     public void next(View view) {
+        info.setText("Real Time Perjalanan");
         FragmentManager fragmentManager = getSupportFragmentManager();
         final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         final Fragment_info_perjalanan fragment_info_perjalanan = new Fragment_info_perjalanan();
 
         fragmentTransaction.replace(R.id.container, fragment_info_perjalanan).commit();
     }
+
+
 }
