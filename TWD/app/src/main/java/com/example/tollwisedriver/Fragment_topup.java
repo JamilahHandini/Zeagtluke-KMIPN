c2004bec833e154b67d6888277cfc24ecbcabbaf
unknown <jamilahhandini8@gmail.com>
2022-07-26 00:17:23 -0700

    konfigurasi firebase + simulasi top-up

---
+++ TWD/app/src/main/java/com/example/tollwisedriver/Fragment_topup.java
@@ -0,0 +1,36 @@
+package com.example.tollwisedriver;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.tollwisedriver.databinding.FragmentTopupBinding;
+
+
+public class Fragment_topup extends Fragment {
+
+    private FragmentTopupBinding binding;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentTopupBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+}
